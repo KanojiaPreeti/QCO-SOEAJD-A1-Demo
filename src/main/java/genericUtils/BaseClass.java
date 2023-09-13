06415a5c21f866d6e2ec70a1a862fbe7fb0f660d
NAKUL HINDUSTANI <nakulhindustani789@gmail.com>
2023-09-13 15:55:27 +0530

    THis is local ID, first test case ID

--- src/main/java/genericUtils/BaseClass.java
+++ src/main/java/genericUtils/BaseClass.java
@@ -4,6 +4,6 @@ public class BaseClass {
 	public static void main(String[] args) {
 	
 		System.out.println("hi this is my first Pull");
-
+        System.out.println("hello changes are there");	
 	}
 }
